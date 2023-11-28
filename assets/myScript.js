9e4b3a1ff45e17ece50595ed42cd505a1e04a3dc
pratham <prathamchande@gmail.com>
2023-11-28 18:50:10 +0530

    animation done

--- assets/myScript.js
+++ assets/myScript.js
@@ -1,19 +1,20 @@
 // Check if the user is accessing from a mobile device
 function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const mobileContainer = document.querySelector('.mobile-container');
-    const desktopMessage = document.querySelector('.desktop-message');
+  const mobileContainer = document.querySelector(".mobile-container");
+  const desktopMessage = document.querySelector(".desktop-message");
 
-    if (isMobile()) {
-        mobileContainer.style.display = 'block';
-    } else {
-        desktopMessage.style.display = 'block';
-    }
+  if (isMobile()) {
+    mobileContainer.style.display = "block";
+  } else {
+    desktopMessage.style.display = "block";
+  }
 });
-
 
 /////////////////////////////////////////////////////////////////////////
 
@@ -23,14 +24,12 @@ document.addEventListener("DOMContentLoaded", function () {
 //     }, 3000000); // Navigate to the next page after 30 seconds
 // });
 
-
 // function stopAnimations() {
 //     const animationImages = document.querySelectorAll('.animationImages');
 //     animationImages.forEach(image => {
 //         image.style.animation = 'none';
 //     });
 // }
-
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     setTimeout(() => {
@@ -42,54 +41,43 @@ document.addEventListener("DOMContentLoaded", function () {
 //     }, 3000000); // Navigate to the next page after 30 seconds
 // });
 
-
-
-
-
 /////////////////////////////////////////////////////////////////////////
 // Navigation code
 
 function redirectToContactPage() {
-    setTimeout(function () {
-        window.location.href = 'contact.html';
-    }, 10000); // 10000 milliseconds = 10 seconds
+  setTimeout(function () {
+    window.location.href = "contact.html";
+  }, 10000); // 10000 milliseconds = 10 seconds
 }
 
 // Function to redirect to info.html
 function navigate(e) {
-
-    if(e == "home"){
-        window.location.href = 'index.html';
-    }
-   else if(e == "info"){
-        window.location.href = 'info.html';
-    }
-    
+  if (e == "home") {
+    window.location.href = "index.html";
+  } else if (e == "info") {
+    window.location.href = "info.html";
+  }
 }
 
-
-
 // Event listener for the submit button in contact.html
-document.addEventListener('DOMContentLoaded', function () {
-
-    // Check if the current page is index.html before calling redirectToContactPage()
-    if (window.location.pathname.endsWith('index.html')) {
-        // Call the function to redirect to contact.html after 10 seconds
-        redirectToContactPage();
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  // Check if the current page is index.html before calling redirectToContactPage()
+  if (window.location.pathname.endsWith("index.html")) {
+    // Call the function to redirect to contact.html after 10 seconds
+    redirectToContactPage();
+    document.body.classList.add("loaded");
+  } else if (window.location.pathname.endsWith("contact.html")) {
+    document.body.classList.add("loaded");
+  } else if (window.location.pathname.endsWith("info.html")) {
+    document.body.classList.add("loaded");
+  }
 });
 
-
-
+/////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////
 
-
 /////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////
-
 
 /////////////////////////////////////////////////////////////////////////
 
