0e62a4cfb357b664cb9963cb23a7935877ddb7f6
pratham <prathamchande@gmail.com>
2023-11-29 00:46:19 +0530

    updated

--- assets/myScript.js
+++ assets/myScript.js
@@ -47,6 +47,13 @@ document.addEventListener("DOMContentLoaded", function () {
 function redirectToContactPage() {
   setTimeout(function () {
     // window.location.href = "contact.html";
+
+    //add class after sometime for display none main
+    // setTimeout(() => {
+    //     document.querySelector('.mainWallpaper').classList.add('dsNone');
+    //         }, 100); 
+
+
   }, 10000); // 10000 milliseconds = 10 seconds
 }
 
@@ -81,6 +88,52 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /////////////////////////////////////////////////////////////////////////
+//text rotation
+
+let words = document.querySelectorAll(".word");
+
+words.forEach(word => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach(letter => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let rotateText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    // Rotate out letters of the current word
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+
+    // Reveal and rotate in letters of the next word
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+rotateText();
+setInterval(rotateText, 4000);
+
+
 
 /////////////////////////////////////////////////////////////////////////
 
