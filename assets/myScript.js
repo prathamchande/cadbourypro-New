204de4c2e1d90c35567088d7a0b7761d2dfb9381
pratham <prathamchande@gmail.com>
2023-12-01 20:10:42 +0530

    upfd

--- assets/myScript.js
+++ assets/myScript.js
@@ -519,52 +519,52 @@ if (window.location.pathname.endsWith("contact.html")) {
 //text rotation
 
 
-  let words = document.querySelectorAll(".word");
+//   let words = document.querySelectorAll(".word");
 
-  words.forEach((word) => {
-    let letters = word.textContent.split("");
-    word.textContent = "";
-    letters.forEach((letter) => {
-      let span = document.createElement("span");
-      span.textContent = letter;
-      span.className = "letter";
-      word.append(span);
-    });
-  });
+//   words.forEach((word) => {
+//     let letters = word.textContent.split("");
+//     word.textContent = "";
+//     letters.forEach((letter) => {
+//       let span = document.createElement("span");
+//       span.textContent = letter;
+//       span.className = "letter";
+//       word.append(span);
+//     });
+//   });
 
-  let currentWordIndex = 0;
-  let maxWordIndex = words.length - 1;
-  words[currentWordIndex].style.opacity = "1";
+//   let currentWordIndex = 0;
+//   let maxWordIndex = words.length - 1;
+//   words[currentWordIndex].style.opacity = "1";
 
-  let rotateText = () => {
-    let currentWord = words[currentWordIndex];
-    let nextWord =
-      currentWordIndex === maxWordIndex
-        ? words[0]
-        : words[currentWordIndex + 1];
+//   let rotateText = () => {
+//     let currentWord = words[currentWordIndex];
+//     let nextWord =
+//       currentWordIndex === maxWordIndex
+//         ? words[0]
+//         : words[currentWordIndex + 1];
 
-    // Rotate out letters of the current word
-    Array.from(currentWord.children).forEach((letter, i) => {
-      setTimeout(() => {
-        letter.className = "letter out";
-      }, i * 80);
-    });
+//     // Rotate out letters of the current word
+//     Array.from(currentWord.children).forEach((letter, i) => {
+//       setTimeout(() => {
+//         letter.className = "letter out";
+//       }, i * 80);
+//     });
 
-    // Reveal and rotate in letters of the next word
-    nextWord.style.opacity = "1";
-    Array.from(nextWord.children).forEach((letter, i) => {
-      letter.className = "letter behind";
-      setTimeout(() => {
-        letter.className = "letter in";
-      }, 340 + i * 80);
-    });
+//     // Reveal and rotate in letters of the next word
+//     nextWord.style.opacity = "1";
+//     Array.from(nextWord.children).forEach((letter, i) => {
+//       letter.className = "letter behind";
+//       setTimeout(() => {
+//         letter.className = "letter in";
+//       }, 340 + i * 80);
+//     });
 
-    currentWordIndex =
-      currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-  };
+//     currentWordIndex =
+//       currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+//   };
 
-  rotateText();
-  setInterval(rotateText, 4000);
+//   rotateText();
+//   setInterval(rotateText, 4000);
 
 
 /////////////////////////////////////////////////////////////////////////
