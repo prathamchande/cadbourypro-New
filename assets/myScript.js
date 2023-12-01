a4d8c8654bf81f6754f5614c60fa67a765419512
pratham <prathamchande@gmail.com>
2023-12-02 01:28:31 +0530

    Done all changes

--- assets/myScript.js
+++ assets/myScript.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
     //console.log("Window height:", window.innerHeight);
 
     if (isMobile()) {
-     // console.log("Switching to mobile view.");
+      // console.log("Switching to mobile view.");
       desktopMessage.style.display = "none";
       mobileContainer.style.display = "block";
     } else {
-    //  console.log("Switching to desktop view.");
+      //  console.log("Switching to desktop view.");
       mobileContainer.style.display = "none";
       desktopMessage.style.display = "block";
     }
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Update display on window resize
   window.addEventListener("resize", function () {
-   // console.log("Window resized.");
+    // console.log("Window resized.");
     updateDisplay();
   });
 
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //for otp input validation
 let digitValidate = function (ele) {
- // console.log(ele.value);
+  // console.log(ele.value);
   ele.value = ele.value.replace(/[^0-9]/g, "");
 };
 
@@ -275,20 +275,21 @@ if (window.location.pathname.endsWith("contact.html")) {
 
     // username mobile number opt validation on first submit button
     if (submitType === "firstSubmit") {
-              // Validate mobile number
-              if (!mobileNumberInput.value) {
-                showNudgePopup("Mobile number cannot be empty.");
-                return;
-              } else if (!/^\d+$/.test(mobileNumberInput.value)) {
-                showNudgePopup("You are not allowed any characters in the mobile number.");
-                return;
-              } else if (mobileNumberInput.value.length !== 10) {
-                showNudgePopup("Mobile number should be 10 digits.");
-                return;
-              } else {
-                hideNudgePopup();
-              }
-
+      // Validate mobile number
+      if (!mobileNumberInput.value) {
+        showNudgePopup("Mobile number cannot be empty.");
+        return;
+      } else if (!/^\d+$/.test(mobileNumberInput.value)) {
+        showNudgePopup(
+          "You are not allowed any characters in the mobile number."
+        );
+        return;
+      } else if (mobileNumberInput.value.length !== 10) {
+        showNudgePopup("Mobile number should be 10 digits.");
+        return;
+      } else {
+        hideNudgePopup();
+      }
 
       // Validate username
       if (!usernameInput.value) {
@@ -303,18 +304,16 @@ if (window.location.pathname.endsWith("contact.html")) {
         hideNudgePopup();
       }
 
-
-
-         // Validate email
-         if (!emailInput.value) {
-            showNudgePopup("Email cannot be empty.");
-            return;
-          } else if (!/\S+@\S+\.\S+/.test(emailInput.value)) {
-            showNudgePopup("Invalid email format.");
-            return;
-          } else {
-            hideNudgePopup();
-          }
+      // Validate email
+      if (!emailInput.value) {
+        showNudgePopup("Email cannot be empty.");
+        return;
+      } else if (!/\S+@\S+\.\S+/.test(emailInput.value)) {
+        showNudgePopup("Invalid email format.");
+        return;
+      } else {
+        hideNudgePopup();
+      }
 
       // Check if both checkboxes are checked
       if (!vehicle1Checkbox.checked || !vehicle2Checkbox.checked) {
@@ -330,7 +329,7 @@ if (window.location.pathname.endsWith("contact.html")) {
         usernameInput.value ||
         emailInput.value ||
         vehicle1Checkbox.checked ||
-        vehicle2Checkbox.checked 
+        vehicle2Checkbox.checked
       ) {
         showModalWithContent("firstModal");
         return;
@@ -339,26 +338,25 @@ if (window.location.pathname.endsWith("contact.html")) {
 
     // Validate second block only when moving from the second to the third block
     if (submitType === "secondSubmit") {
-        if (selectedlovedNameId.value === "") {
-            showNudgePopup("Please select a Loved Name please.");
-            return;
-          } else {
-            hideNudgePopup();
-          }
-        if (selectedlovedAge.value === "") {
-            showNudgePopup("Please select a Age");
-            return;
-          } else {
-            hideNudgePopup();
-          }
-          if (selectedlovedGender.value === "") {
-            showNudgePopup("Please select a Gender");
-            return;
-          } else {
-            hideNudgePopup();
-          }
-    } 
-
+      if (selectedlovedNameId.value === "") {
+        showNudgePopup("Please select a Loved Name please.");
+        return;
+      } else {
+        hideNudgePopup();
+      }
+      if (selectedlovedAge.value === "") {
+        showNudgePopup("Please select a Age");
+        return;
+      } else {
+        hideNudgePopup();
+      }
+      if (selectedlovedGender.value === "") {
+        showNudgePopup("Please select a Gender");
+        return;
+      } else {
+        hideNudgePopup();
+      }
+    }
 
     // If all validations pass, proceed with navigation
     navigate(submitType);
@@ -429,48 +427,49 @@ if (window.location.pathname.endsWith("contact.html")) {
       containerIds[nextContainerIndex]
     );
     var nextButton = document.getElementById(buttonIds[nextContainerIndex]);
-    var nextProgressBar = document.getElementById(progressBarIds[nextContainerIndex]);
+    var nextProgressBar = document.getElementById(
+      progressBarIds[nextContainerIndex]
+    );
     nextContainer.classList.add("hiddenCont");
 
-      // Reload the page before navigating to the next container if coming from seventh
-  if (submitType === "seventhSubmit") {
-   // window.location.reload();
-   window.location.href = "index.html";
-    return; // Stop further execution
-  }
-//   if (submitType === "fifthSubmit") {
-//     // window.location.reload();
+    // Reload the page before navigating to the next container if coming from seventh
+    if (submitType === "seventhSubmit") {
+      // window.location.reload();
+      window.location.href = "index.html";
+      return; // Stop further execution
+    }
+    //   if (submitType === "fifthSubmit") {
+    //     // window.location.reload();
 
-//     setTimeout(() => {
-//         document.getElementById("forGenrateSong").classList.remove("visiNone");
-//       }, 1000);
-    
-//      return; // Stop further execution
-//    }
-    var childElementsFirst = nextContainer.querySelectorAll(".imgWallpaperWrap");
+    //     setTimeout(() => {
+    //         document.getElementById("forGenrateSong").classList.remove("visiNone");
+    //       }, 1000);
+
+    //      return; // Stop further execution
+    //    }
+    var childElementsFirst =
+      nextContainer.querySelectorAll(".imgWallpaperWrap");
     var childElementsSecond = nextContainer.querySelectorAll(".bgWll");
     var childElementsThird = nextContainer.querySelectorAll(".bagWall");
-    
+
     applyClassToChildElements(childElementsFirst, "animate");
     applyClassToChildElements(childElementsSecond, "now");
     applyClassToChildElements(childElementsThird, "check");
-    
+
     function applyClassToChildElements(childElements, className) {
       if (!childElements || childElements.length === 0) {
         console.warn("No matching elements found.");
         return;
       }
-    
+
       childElements.forEach(function (childElement) {
         setTimeout(() => {
-            childElement.classList.toggle(className);
-          }, 100);
-       
+          childElement.classList.toggle(className);
+        }, 100);
       });
     }
 
     if (!nextContainer.classList.contains("activeCont")) {
-        
       nextContainer.classList.add("activeCont");
     }
 
@@ -563,56 +562,91 @@ if (window.location.pathname.endsWith("index.html")) {
 /////////////////////////////////////////////////////////////////////////
 //testing purpose
 function navForIndex() {
-    window.location.href = "index.html";
+  window.location.href = "index.html";
 }
 /////////////////////////////////////////////////////////////////////////
 function toggleMoodVibe(clickedElement) {
-    // Remove the 'selected' class from all elements with class 'imgBgVib'
-    var allImgBgVibElements = document.querySelectorAll('.imgBgVib');
-    var allImgBgVibElements2 = document.querySelectorAll('.imgBgVib2');
-    var allImgBgVibElements3 = document.querySelectorAll('.imgBgVib3');
+  // Remove the 'selected' class from all elements with class 'imgBgVib'
+  var allImgBgVibElements = document.querySelectorAll(".imgBgVib");
+  var allImgBgVibElements2 = document.querySelectorAll(".imgBgVib2");
+  var allImgBgVibElements3 = document.querySelectorAll(".imgBgVib3");
 
-    if(allImgBgVibElements){
-        allImgBgVibElements.forEach(function(element) {
-            element.classList.remove('selected');
-        });
-    } else if(allImgBgVibElements2){
-        allImgBgVibElements.forEach(function(element) {
-            element.classList.remove('selected');
-        });
-    } else  if(allImgBgVibElements3){
-        allImgBgVibElements.forEach(function(element) {
-            element.classList.remove('selected');
-        });
-    }
-   
-
-    // Add the 'selected' class only to the clicked element
-    clickedElement.classList.add('selected');
-}
-
-function toggleMoodVibe(clickedElement) {
-    // Get the parent block of the clicked element
-    var parentBlock = clickedElement.closest('.vibeBlock');
-
-    // Remove the 'selected' class from all elements within the same block
-    var allElements = parentBlock.querySelectorAll('.imgBgVib, .imgBgVib2, .imgBgVib3');
-    allElements.forEach(function(element) {
-        element.classList.remove('selected');
+  if (allImgBgVibElements) {
+    allImgBgVibElements.forEach(function (element) {
+      element.classList.remove("selected");
     });
+  } else if (allImgBgVibElements2) {
+    allImgBgVibElements.forEach(function (element) {
+      element.classList.remove("selected");
+    });
+  } else if (allImgBgVibElements3) {
+    allImgBgVibElements.forEach(function (element) {
+      element.classList.remove("selected");
+    });
+  }
 
-    // Add the 'selected' class only to the clicked element
-    clickedElement.classList.add('selected');
+  // Add the 'selected' class only to the clicked element
+  clickedElement.classList.add("selected");
 }
 
+function toggleMoodVibe(clickedElement) {
+  // Get the parent block of the clicked element
+  var parentBlock = clickedElement.closest(".vibeBlock");
 
+  // Remove the 'selected' class from all elements within the same block
+  var allElements = parentBlock.querySelectorAll(
+    ".imgBgVib, .imgBgVib2, .imgBgVib3"
+  );
+  allElements.forEach(function (element) {
+    element.classList.remove("selected");
+  });
 
-
+  // Add the 'selected' class only to the clicked element
+  clickedElement.classList.add("selected");
+}
 
 /////////////////////////////////////////////////////////////////////////
+// Get the fifthMainContBtn element
+// Function to handle changes in the "btnBlockShow" class
+function handleClassChange(mutations) {
+  mutations.forEach(function (mutation) {
+    if (
+      mutation.attributeName === "class" &&
+      mutation.target.classList.contains("btnBlockShow")
+    ) {
+      // Add the footerBg class to mainFooter
+      document.getElementById("footerId").classList.add("footerBg");
+    } else {
+      // Remove the footerBg class from mainFooter
+      document.getElementById("footerId").classList.remove("footerBg");
+    }
+  });
+}
 
+// Create a MutationObserver to observe changes in the "btnBlockShow" class
+var observer = new MutationObserver(handleClassChange);
+
+// Target the element with the ID "fifthMainContBtnID"
+var targetElement = document.getElementById("thirdMainContBtnID");
+
+// Configure the observer to watch for attribute changes
+var observerConfig = { attributes: true };
+
+// Start observing the target element
+observer.observe(targetElement, observerConfig);
 /////////////////////////////////////////////////////////////////////////
 
+var answerMoreButton = document.getElementById('answrMoreId');
+function scrollContainerDown() {
+  var container = document.getElementById("tellMoreID");
+  container.scrollTop = container.scrollHeight;
+  // Toggle classes on the Answer More button
+  answerMoreButton.classList.remove("answerMore");
+  answerMoreButton.classList.add("deactiveBtn");
+
+  // Disable the button after it has been clicked
+  answerMoreButton.disabled = true;
+}
 /////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////
